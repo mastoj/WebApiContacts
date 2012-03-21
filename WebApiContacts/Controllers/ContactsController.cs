63b12e47f20a2381456ffd38315f84ba8c09199b
Tomas Jansson <tomas.jansson@gmail.com>
2012-03-21 22:29:09 +0100

    forgot to save everything

--- WebApiContacts/Controllers/ContactsController.cs
+++ WebApiContacts/Controllers/ContactsController.cs
@@ -66,23 +66,4 @@ namespace WebApiContacts.Controllers
             return response;
         }
     }
-
-    public static class HttpContentExtensions
-    {
-         public static string TryGetFormFieldValue(this IEnumerable<HttpContent> contents, string dispositionName, string defaultValue = "")    
-         {    
-             if (contents == null)    
-             {    
-                 throw new ArgumentNullException("contents");    
-             }    
-             
-             HttpContent content = contents.FirstDispositionNameOrDefault(dispositionName);    
-             if (content != null)   
-             {   
-                 var formFieldValue = content.ReadAsStringAsync().Result;   
-                 return formFieldValue;   
-             }   
-             return defaultValue;   
-         }
-    }
 }
