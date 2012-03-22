1e95d673c217b3da42afaddc6290003faa9a3263
Tomas Jansson <tomas.jansson@gmail.com>
2012-03-22 23:42:23 +0100

    added another semicolor

--- WebApiContacts/Scripts/webapicontacts.js
+++ WebApiContacts/Scripts/webapicontacts.js
@@ -1,18 +1,18 @@
 ﻿var httpStuff = {
     statusCodes:
-                {
-                    OK: 200,
-                    Created: 201,
-                    Accepted: 202,
-                    NoContent: 204,
-                    BadRequest: 400,
-                    Unauthorized: 401,
-                    Forbidden: 403,
-                    NotFound: 404,
-                    NotAllowed: 405,
-                    ServerError: 500
-                }
-}
+        {
+            OK: 200,
+            Created: 201,
+            Accepted: 202,
+            NoContent: 204,
+            BadRequest: 400,
+            Unauthorized: 401,
+            Forbidden: 403,
+            NotFound: 404,
+            NotAllowed: 405,
+            ServerError: 500
+        }
+};
 function ViewModel() {
     var self = this;
     self.contacts = ko.observableArray();
